d0d4ca8c652ba4c85e388e5cffc0d70a64d25742
Kenneth Glimberg Christensen <kenneth.glimberg.christensen@gmail.com>
2025-01-26 13:02:13 +0100

    Finish solving day 15 part 1 - performance optimization

--- AdventOfCode/Day15WarehouseWoes1/Day15WarehouseWoes1.cpp
+++ AdventOfCode/Day15WarehouseWoes1/Day15WarehouseWoes1.cpp
@@ -19,352 +19,286 @@
 #include <vector>
 #include <algorithm>
 
-struct tile {
-	int x;
-	int y;
-	char t;
+struct Tile {
+    int x;
+    int y;
+    char t;
 
-	bool operator<(const tile& rhs) const {
-		return rhs.x < this->x || (rhs.x == this->x && rhs.y < this->y);
-	}
+    bool operator<(const Tile& rhs) const {
+        return rhs.x < this->x || (rhs.x == this->x && rhs.y < this->y);
+    }
 };
 
-
-
-int main()
-{
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-
-	auto start = std::chrono::high_resolution_clock::now();
-
-	char chTile;
-	long sumOfAllBoxesGPSCoordinates = 0;
-	std::vector<tile> warehouse;
-	std::queue<char> moves;
-
-	std::ifstream inputfile("input.txt");
-
-	if (!inputfile.is_open()) { std::cerr << "Cannot open file!\n"; return 0; }
-
-	int x = 0;
-	int y = 0;
-	int startX = 0;
-	int startY = 0;
-	int width = 0;  // X >>>>>
-	int height = 0; // Y vvvvv
-
-	while (inputfile.get(chTile)) {
-
-		if (chTile == '\n') {
-			y++;
-			x = 0;
-			continue;
-		}
-		else if (chTile == '@') {
-			startX = x;
-			startY = y;
-		}
-		else if (chTile == '#' || chTile == 'O') {
-			width = x;
-			height = y;
-			warehouse.emplace_back(tile{ x,y,chTile });
-		}
-		else if (chTile == '<' || chTile == '^' || chTile == 'v' || chTile == '>') {
-			moves.emplace(chTile);
-		}
-
-
-		x++;
-
-		//std::cout << tile << "\n";
-	}
-
-
-
-	for (int idxY = 0; idxY <= height; idxY++) {
-		for (int idxX = 0; idxX <= width; idxX++) {
-
-			auto foundTile = std::find_if(warehouse.begin(), warehouse.end(), [&](const tile& t) {
-				return (t.x == idxX && t.y == idxY);
-				});
-
-			if (foundTile != warehouse.end()) {
-				std::cout << foundTile->t;
-			}
-			else {
-				if (startX == idxX && startY == idxY) {
-					std::cout << "@";
-				}
-				else {
-					std::cout << ".";
-				}
-			}
-
-		}
-		std::cout << "\n";
-
-	}
-
-
-
-
-	while (!moves.empty()) {
-		char move = moves.front();
-
-		//std::cout << "Move: " << move << "\n";
-		
-		if (move == '<') {
-
-			auto nearestWall = std::find_if(warehouse.rbegin(), warehouse.rend(), [&](const tile& f) {
-				return (f.x < startX && f.y == startY && f.t == '#');
-				});
-
-			int distanceFromX = std::abs(startX - nearestWall->x);
-
-			std::stack<int> boxes;
-
-			for (int idx = startX - 1; idx > (startX - distanceFromX); --idx) {
-				auto isBox = std::find_if(warehouse.rbegin(), warehouse.rend(), [&](const tile& f) {
-					return (f.x == idx && f.y == startY && f.t == 'O');
-					});
-
-				if (isBox != warehouse.rend())
-				{
-					boxes.emplace(idx);
-				}
-				else {
-					idx = 0;
-				}
-
-			}
-
-			startX = std::max((nearestWall->x + 1 + static_cast<int>(boxes.size())), startX - 1);
-
-			int counter = 1;
-
-			while (!boxes.empty()) {
-				auto idx = boxes.top();
-
-				auto theBox = std::find_if(warehouse.begin(), warehouse.end(), [&](const tile& f) {
-					return (f.x == idx && f.y == startY && f.t == 'O');
-					});
-
-				theBox->x = std::max(theBox->x - 1, nearestWall->x + counter);
-
-				boxes.pop();
-				counter++;
-			}
-
-		} 
-		else if (move == '^') {
-
-			auto nearestWall = std::find_if(warehouse.rbegin(), warehouse.rend(), [&](const tile& f) {
-				return (f.x == startX && f.y < startY && f.t == '#');
-				});
-
-			int distanceFromY = std::abs(startY - nearestWall->y);
-
-			std::stack<int> boxes;
-
-			for (int idy = startY - 1; idy > (startY - distanceFromY); --idy) {
-				auto isBox = std::find_if(warehouse.rbegin(), warehouse.rend(), [&](const tile& f) {
-					return (f.x == startX && f.y == idy && f.t == 'O');
-					});
-
-				if (isBox != warehouse.rend())
-				{
-					boxes.emplace(idy);
-				}
-				else {
-					idy = 0;
-				}
-
-			}
-
-			startY = std::max((nearestWall->y + 1 + static_cast<int>(boxes.size())), startY - 1);
-
-
-			int counter = 1;
-
-			while (!boxes.empty()) {
-				auto idy = boxes.top();
-
-				auto theBox = std::find_if(warehouse.begin(), warehouse.end(), [&](const tile& f) {
-					return (f.x == startX && f.y == idy && f.t == 'O');
-					});
-
-				theBox->y = std::max(theBox->y - 1, nearestWall->y + counter);
-
-				boxes.pop();
-				counter++;
-			}
-
-		}
-		else if (move == '>') {
-
-			auto nearestWall = std::find_if(warehouse.begin(), warehouse.end(), [&](const tile& f) {
-				return (f.x > startX && f.y == startY && f.t == '#');
-				});
-
-			int distanceFromX = std::abs(startX - nearestWall->x);
-
-			std::stack<int> boxes;
-
-			for (int idx = startX + 1; idx < (startX + distanceFromX); ++idx) {
-				auto isBox = std::find_if(warehouse.begin(), warehouse.end(), [&](const tile& f) {
-					return (f.x == idx && f.y == startY && f.t == 'O');
-					});
-
-				if (isBox != warehouse.end())
-				{
-					boxes.emplace(idx);
-				}
-				else {
-					idx = nearestWall->x;
-				}
-
-			}
-
-			startX = std::min((nearestWall->x - 1 - static_cast<int>(boxes.size())), startX + 1);
-
-			int counter = 1;
-
-			while (!boxes.empty()) {
-				auto idx = boxes.top();
-
-				auto theBox = std::find_if(warehouse.begin(), warehouse.end(), [&](const tile& f) {
-					return (f.x == idx && f.y == startY && f.t == 'O');
-					});
-
-				theBox->x = std::min(theBox->x + 1, nearestWall->x - counter);
-				
-				boxes.pop();
-				counter++;
-			}
-
-			
-
-			
-
-		}
-		else if (move == 'v') {
-
-			auto nearestWall = std::find_if(warehouse.begin(), warehouse.end(), [&](const tile& f) {
-				return (f.x == startX && f.y > startY && f.t == '#');
-				});
-
-			int distanceFromY = std::abs(startY - nearestWall->y);
-
-			std::stack<int> boxes;
-
-			for (int idy = startY + 1; idy < (startY + distanceFromY); ++idy) {
-				auto isBox = std::find_if(warehouse.begin(), warehouse.end(), [&](const tile& f) {
-					return (f.x == startX && f.y == idy && f.t == 'O');
-					});
-
-				if (isBox != warehouse.end())
-				{
-					boxes.emplace(idy);
-				}
-				else {
-					idy = nearestWall->y;
-				}
-
-			}
-
-			startY = std::min((nearestWall->y - 1 - static_cast<int>(boxes.size())), startY + 1);
-			
-
-			int counter = 1;
-
-
-
-			while (!boxes.empty()) {
-				auto idy = boxes.top();
-
-				auto theBox = std::find_if(warehouse.begin(), warehouse.end(), [&](const tile& f) {
-					return (f.x == startX && f.y == idy && f.t == 'O');
-					});
-
-				theBox->y = std::min(theBox->y + 1, nearestWall->y - counter);
-				//startY = nearestWall->y - counter - 1;
-				boxes.pop();
-				counter++;
-
-			}
-
-		}
-
-		//for (int idxY = 0; idxY <= height; idxY++) {
-		//	for (int idxX = 0; idxX <= width; idxX++) {
-
-		//		auto foundTile = std::find_if(warehouse.begin(), warehouse.end(), [&](const tile& t) {
-		//			return (t.x == idxX && t.y == idxY);
-		//			});
-
-
-
-		//		if (foundTile != warehouse.end()) {
-		//			std::cout << foundTile->t;
-
-		//		}
-		//		else {
-		//			if (startX == idxX && startY == idxY) {
-		//				std::cout << "@";
-		//			}
-		//			else {
-		//				std::cout << ".";
-		//			}
-		//		}
-
-		//	}
-		//	std::cout << "\n";
-
-		//}
-
-		moves.pop();
-	}
-
-	for (int idxY = 0; idxY <= height; idxY++) {
-		for (int idxX = 0; idxX <= width; idxX++) {
-
-			auto foundTile = std::find_if(warehouse.begin(), warehouse.end(), [&](const tile& t) {
-				return (t.x == idxX && t.y == idxY);
-				});
-
-
-
-			if (foundTile != warehouse.end()) {
-				std::cout << foundTile->t;
-
-				if (foundTile->t == 'O')
-					sumOfAllBoxesGPSCoordinates += (100 * foundTile->y) + foundTile->x;
-
-			}
-			else {
-				if (startX == idxX && startY == idxY) {
-					std::cout << "@";
-				}
-				else {
-					std::cout << ".";
-				}
-			}
-
-		}
-		std::cout << "\n";
-
-	}
-
-	inputfile.close();
-
-	std::cout << "Sum of all boxes' GPS coordinates: " << sumOfAllBoxesGPSCoordinates << "\n";
-
-	auto stop = std::chrono::high_resolution_clock::now();
-
-	auto duration = duration_cast<std::chrono::microseconds>(stop - start);
-
-	std::cout << "Time taken by function: "
-		<< duration.count() << " microseconds\n";
+struct dir {
+    int dx;
+    int dy;
+};
+
+static dir directions[] = {
+    {-1, 0}, // Up
+    {0, 1}, // Right
+    {1, 0}, // Down
+    {0, -1} // Left
+
+};
+
+int main() {
+    auto start = std::chrono::high_resolution_clock::now();
+
+    char chTile;
+    long sumOfAllBoxesGPSCoordinates = 0;
+    std::vector<Tile> warehouse;
+    std::queue<char> moves;
+    std::map<int, std::vector<int>> wallsX, wallsY;
+
+    std::ifstream inputfile("input.txt");
+
+    if (!inputfile.is_open()) {
+        std::cerr << "Cannot open file!\n";
+        return 0;
+    }
+
+    int x = 0, y = 0, startX = 0, startY = 0, width = 0, height = 0;
+
+    while (inputfile.get(chTile)) {
+        if (chTile == '\n') {
+            y++;
+            x = 0;
+            continue;
+        }
+
+        switch (chTile) {
+        case '@':
+            startX = x;
+            startY = y;
+            break;
+        case '#':
+        case 'O':
+            width = x;
+            height = y;
+            warehouse.emplace_back(Tile{ x, y, chTile });
+            if (chTile == '#') {
+                wallsX[y].push_back(x);
+                wallsY[x].push_back(y);
+            }
+            break;
+        case '<':
+        case '^':
+        case 'v':
+        case '>':
+            moves.emplace(chTile);
+            break;
+        }
+        x++;
+    }
+
+    auto findNearestWallX = [&wallsX](int y, int startX, bool left) {
+        auto& walls = wallsX[y];
+        if (left) {
+            auto it = std::lower_bound(walls.rbegin(), walls.rend(), startX, std::greater<int>());
+            return (it == walls.rend()) ? -1 : *it;
+        }
+        else {
+            auto it = std::lower_bound(walls.begin(), walls.end(), startX);
+            return (it == walls.end()) ? -1 : *it;
+        }
+        };
+
+    auto findNearestWallY = [&wallsY](int x, int startY, bool up) {
+        auto& walls = wallsY[x];
+        if (up) {
+            auto it = std::lower_bound(walls.rbegin(), walls.rend(), startY, std::greater<int>());
+            return (it == walls.rend()) ? -1 : *it;
+        }
+        else {
+            auto it = std::lower_bound(walls.begin(), walls.end(), startY);
+            return (it == walls.end()) ? -1 : *it;
+        }
+        };
+
+    while (!moves.empty()) {
+        char move = moves.front();
+        std::stack<int> boxes;
+
+        if (move == '<') {
+            int nearestWallX = findNearestWallX(startY, startX, true);
+            if (nearestWallX == -1) nearestWallX = 0;
+            int distanceFromX = std::abs(startX - nearestWallX);
+
+            for (int idx = startX - 1; idx > (startX - distanceFromX); --idx) {
+                auto isBox = std::find_if(warehouse.rbegin(), warehouse.rend(), [&](const Tile& f) {
+                    return (f.x == idx && f.y == startY && f.t == 'O');
+                    });
+
+                if (isBox != warehouse.rend()) {
+                    boxes.emplace(idx);
+                }
+                else {
+                    break;
+                }
+            }
+
+            startX = std::max((nearestWallX + 1 + static_cast<int>(boxes.size())), startX - 1);
+
+            int counter = 1;
+            while (!boxes.empty()) {
+                auto idx = boxes.top();
+
+                auto theBox = std::find_if(warehouse.begin(), warehouse.end(), [&](const Tile& f) {
+                    return (f.x == idx && f.y == startY && f.t == 'O');
+                    });
+
+                theBox->x = std::max(theBox->x - 1, nearestWallX + counter);
+
+                boxes.pop();
+                counter++;
+            }
+        }
+        else if (move == '^') {
+            int nearestWallY = findNearestWallY(startX, startY, true);
+            if (nearestWallY == -1) nearestWallY = 0;
+            int distanceFromY = std::abs(startY - nearestWallY);
+
+            for (int idy = startY - 1; idy > (startY - distanceFromY); --idy) {
+                auto isBox = std::find_if(warehouse.rbegin(), warehouse.rend(), [&](const Tile& f) {
+                    return (f.x == startX && f.y == idy && f.t == 'O');
+                    });
+
+                if (isBox != warehouse.rend()) {
+                    boxes.emplace(idy);
+                }
+                else {
+                    break;
+                }
+            }
+
+            startY = std::max((nearestWallY + 1 + static_cast<int>(boxes.size())), startY - 1);
+
+            int counter = 1;
+            while (!boxes.empty()) {
+                auto idy = boxes.top();
+
+                auto theBox = std::find_if(warehouse.begin(), warehouse.end(), [&](const Tile& f) {
+                    return (f.x == startX && f.y == idy && f.t == 'O');
+                    });
+
+                theBox->y = std::max(theBox->y - 1, nearestWallY + counter);
+
+                boxes.pop();
+                counter++;
+            }
+        }
+        else if (move == '>') {
+            int nearestWallX = findNearestWallX(startY, startX, false);
+            if (nearestWallX == -1) nearestWallX = warehouse.size();
+            int distanceFromX = std::abs(startX - nearestWallX);
+
+            for (int idx = startX + 1; idx < (startX + distanceFromX); ++idx) {
+                auto isBox = std::find_if(warehouse.begin(), warehouse.end(), [&](const Tile& f) {
+                    return (f.x == idx && f.y == startY && f.t == 'O');
+                    });
+
+                if (isBox != warehouse.end()) {
+                    boxes.emplace(idx);
+                }
+                else {
+                    break;
+                }
+            }
+
+            startX = std::min((nearestWallX - 1 - static_cast<int>(boxes.size())), startX + 1);
+
+            int counter = 1;
+            while (!boxes.empty()) {
+                auto idx = boxes.top();
+
+                auto theBox = std::find_if(warehouse.begin(), warehouse.end(), [&](const Tile& f) {
+                    return (f.x == idx && f.y == startY && f.t == 'O');
+                    });
+
+                theBox->x = std::min(theBox->x + 1, nearestWallX - counter);
+
+                boxes.pop();
+                counter++;
+            }
+        }
+        else if (move == 'v') {
+            int nearestWallY = findNearestWallY(startX, startY, false);
+            if (nearestWallY == -1) nearestWallY = warehouse.size();
+            int distanceFromY = std::abs(startY - nearestWallY);
+
+            for (int idy = startY + 1; idy < (startY + distanceFromY); ++idy) {
+                auto isBox = std::find_if(warehouse.begin(), warehouse.end(), [&](const Tile& f) {
+                    return (f.x == startX && f.y == idy && f.t == 'O');
+                    });
+
+                if (isBox != warehouse.end()) {
+                    boxes.emplace(idy);
+                }
+                else {
+                    break;
+                }
+            }
+
+            startY = std::min((nearestWallY - 1 - static_cast<int>(boxes.size())), startY + 1);
+
+            int counter = 1;
+            while (!boxes.empty()) {
+                auto idy = boxes.top();
+
+                auto theBox = std::find_if(warehouse.begin(), warehouse.end(), [&](const Tile& f) {
+                    return (f.x == startX && f.y == idy && f.t == 'O');
+                    });
+
+                theBox->y = std::min(theBox->y + 1, nearestWallY - counter);
+
+                boxes.pop();
+                counter++;
+            }
+        }
+        moves.pop();
+    }
+
+    for (int idxY = 0; idxY <= height; idxY++) {
+        for (int idxX = 0; idxX <= width; idxX++) {
+
+            auto foundTile = std::find_if(warehouse.begin(), warehouse.end(), [&](const Tile& t) {
+                return (t.x == idxX && t.y == idxY);
+                });
+
+
+
+            if (foundTile != warehouse.end()) {
+                //std::cout << foundTile->t;
+
+                if (foundTile->t == 'O')
+                    sumOfAllBoxesGPSCoordinates += (100 * foundTile->y) + foundTile->x;
+
+            }
+            //else {
+            //	if (startX == idxX && startY == idxY) {
+            //		std::cout << "@";
+            //	}
+            //	else {
+            //		std::cout << ".";
+            //	}
+            //}
+
+        }
+        //std::cout << "\n";
+
+    }
+
+    inputfile.close();
+
+    std::cout << "Sum of all boxes' GPS coordinates: " << sumOfAllBoxesGPSCoordinates << "\n";
+
+    auto stop = std::chrono::high_resolution_clock::now();
+
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+    std::cout << "Time taken by function: "
+        << duration.count() << " microseconds\n";
 }
 
 
